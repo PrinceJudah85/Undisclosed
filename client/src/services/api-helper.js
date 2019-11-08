781dd1c5444c0d0a0695ff0e61838e3017a19929
Nehemias Cruz <nehemiascruz@Nehemiass-MacBook-Pro.local>
2019-11-08 16:47:09 -0500

    users can register, login, and logout

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -8,25 +8,26 @@ const api = axios.create({
 // ============== Auth ================
 export const registerUser = async (registerData) => {
   try {
-    const response = await api.post('/auth/register', registerData)
+    const response = await api.post('/users/register', registerData)
     api.defaults.headers.common.authorization = `Bearer ${response.data.token}`
     localStorage.setItem('authToken', response.data.token)
+    debugger;
     return response.data.user
 
   } catch (e) {
-    return {error: "invalid credentials"}
+    return { error: "invalid credentials" }
   }
 }
 
 export const loginUser = async (loginData) => {
   try {
-    const response = await api.post('/auth/login', loginData)
+    const response = await api.post('/users/login', loginData)
     api.defaults.headers.common.authorization = `Bearer ${response.data.token}`
     localStorage.setItem('authToken', response.data.token)
     return response.data.user
 
   } catch (e) {
-    return {error: "invalid credentials"}
+    return { error: "invalid credentials" }
   }
 }
 
@@ -34,8 +35,10 @@ export const verifyUser = async () => {
   const token = localStorage.authToken;
   if (token) {
     api.defaults.headers.common.authorization = `Bearer ${token}`
-    const response = await api.get('/auth/verify')
+    const response = await api.get('/users/verify')
     return response.data
   }
   return false
 }
+
+
