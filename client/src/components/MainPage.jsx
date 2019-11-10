70591e094c7e4bcfd1d23a7544987664e882ee11
Raul Jiminian <RJiminian@iBooks-MacBook-Pro.local>
2019-11-10 17:35:30 -0500

    CSS and Welcome page update

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -5,17 +5,21 @@ export default class MainPage extends React.Component {
 
   render() {
     return (
-      <div>
+      <div id="main-div">
         <div id="blog-list">
           {
             this.props.blogs.map(blog => (
               <div className="blogs" key={blog.id}>
-                <img src={blog.image_url} alt="blog-picture" />
-                <h2>Author: {this.props.currentUser.username}</h2>
-                <h4>Content: {blog.content}</h4>
-                <Link to={`/blogs/${this.props.blog_id}`}>
-                  <button onClick={() => this.props.handleClick(blog.id)}>...read more!</button>
-                </Link>
+                <div>
+                  <img src={blog.image_url} alt="blog-post" />
+                </div>
+                <div>
+                  <h2>Author: {this.props.currentUser.username}</h2>
+                  <h4>Content: {blog.content}</h4>
+                  <Link to={`/blogs/${this.props.blog_id}`}>
+                    <button onClick={() => this.props.handleClick(blog.id)}>...read more!</button>
+                  </Link>
+                </div>
               </div>
             ))
           }
