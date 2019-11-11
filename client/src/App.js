7ff71a5c5a442f7f8a28090711033ec75f56154f
Raul Jiminian <RJiminian@iBooks-MacBook-Pro.local>
2019-11-11 17:44:46 -0500

    Team changes updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Route, Link, withRouter } from 'react-router-dom';
-import { registerUser, loginUser, verifyUser, getAllBlogs, getAllUserBlogs } from './services/api-helper';
+import { registerUser, loginUser, verifyUser, getAllBlogs, getAllUserBlogs, getOneBlog, postBlog } from './services/api-helper';
 import Welcome from './components/Welcome';
 import Header from './components/Header';
 import RegisterForm from './components/RegisterForm';
 import LoginForm from './components/LoginForm';
+import CreateBlog from './components/CreateBlog';
 import MainPage from './components/MainPage';
+import FullBlog from './components/FullBlog';
 import Footer from './components/Footer';
+// import UserBlogs from './components/UserBlogs';     <---[TBU] need to create the route for this
 
 class App extends React.Component {
   constructor() {
@@ -16,11 +19,17 @@ class App extends React.Component {
       currentUser: null,
       authErrorMessage: '',
       blogs: [],
-      currentUserBlogs: []
+      currentUserBlogs: [],
+      oneBlog: {}
     }
   }
 
-
+  handleClick = async (e) => {
+    const id = parseInt(e.target.id);
+    const oneBlog = await getOneBlog(id)
+    this.setState({ oneBlog })
+    console.log(this.state.oneBlog)
+  }
 
   handleLogin = async (loginData) => {
     const currentUser = await loginUser(loginData);
@@ -77,6 +86,17 @@ class App extends React.Component {
     })
   }
 
+  // =========== CREATE BLOG FUNCTION ============
+
+  createBlog = async (blogData) => {
+    const response = await postBlog(blogData);
+    const newBlog = response.data
+    this.setState(prevState => ({
+      blogs: [...prevState.blogs, newBlog]
+    }))
+    this.props.history.push('/blogs')
+  }
+
   async componentDidMount() {
     const currentUser = await this.handleVerify();
     this.readAllBlogs();
@@ -92,12 +112,14 @@ class App extends React.Component {
         <Header />
         {
           this.state.currentUser ?
-            <Route exact path="/blogs" render={() => (<MainPage blogs={this.state.blogs} currentUser={this.state.currentUser} currentUserBlogs={this.state.currentUserBlogs} handleLogout={this.handleLogout} />)} /> :
+            <Route exact path="/blogs" render={() => (<MainPage oneBlog={this.state.oneBlog} handleClick={this.handleClick} blogs={this.state.blogs} currentUser={this.state.currentUser} currentUserBlogs={this.state.currentUserBlogs} handleLogout={this.handleLogout} />)} /> :
             <Link to="/login"><button>Login/Register</button></Link>
         }
         <Footer />
         <Route path="/login" render={() => (<LoginForm handleLogin={this.handleLogin} authErrorMessage={this.state.authErrorMessage} />)} />
         <Route path='/register' render={() => (<RegisterForm handleRegister={this.handleRegister} authErrorMessage={this.state.authErrorMessage} />)} />
+        <Route path="/full_blog/:id" render={() => (<FullBlog oneBlog={this.state.oneBlog} />)} />
+        <Route path='/blogs/new' render={() => (<CreateBlog createBlog={this.createBlog} />)} />
       </div>
     );
   }
