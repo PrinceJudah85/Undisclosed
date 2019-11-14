5ea2f625e265a51d0d65c9ff2f9f2493dab43250
Rafael Castano <rafa328@gmail.com>
2019-11-14 17:08:42 -0500

    updated Base URL to new heroku site

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:3000"
+  baseURL: "https://desolate-mountain-14579.herokuapp.com/"
 });
 
 // ============== Auth ================
